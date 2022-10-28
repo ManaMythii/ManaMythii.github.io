98a0f265f59715586c709ea0d9465aac32674a80
ManaMythii <105987369+ManaMythii@users.noreply.github.com>
2022-10-28 11:47:03 -0500

    Create blessing-data.js

---
+++ js/data/blessing-data.js
@@ -0,0 +1,107 @@
+module.exports = [
+  {
+    hero: "Fjorm",
+    type: "Water",
+    stats: { hp: 3, spd: 3 },
+    icon: 0
+  },
+  {
+    hero: "Gunnthrá",
+    type: "Wind",
+    stats: { hp: 3, res: 4 },
+    icon: 1
+  },
+  {
+    hero: "Ike (Legendary Heroes)",
+    type: "Earth",
+    stats: { hp: 3, atk: 2 },
+    icon: 2
+  },
+  {
+    hero: "Ephraim (Legendary Heroes)",
+    type: "Fire",
+    stats: { hp: 3, def: 4 },
+    icon: 3
+  },
+  {
+    hero: "Robin (F) (Legendary Heroes)",
+    type: "Earth",
+    stats: { hp: 3, spd: 3 },
+    icon: 4
+  },
+  {
+    hero: "Lyn (Legendary Heroes)",
+    type: "Wind",
+    stats: { hp: 3, atk: 2 },
+    icon: 5
+  },
+  {
+    hero: "Ryoma (Legendary Heroes)",
+    type: "Water",
+    stats: { hp: 3, def: 4 },
+    icon: 6
+  },
+  {
+    hero: "Hector (Legendary Heroes)",
+    type: "Fire",
+    stats: { hp: 3, atk: 2 },
+    icon: 7
+  },
+  {
+    hero: "Lucina (Legendary Heroes)",
+    type: "Wind",
+    stats: { hp: 3, spd: 3 },
+    icon: 8
+  },
+  {
+    hero: "Marth (Legendary Heroes)",
+    type: "Fire",
+    stats: { hp: 3, res: 4 },
+    icon: 9
+  },
+  {
+    hero: "Tiki (Young) (Legendary Heroes)",
+    type: "Earth",
+    stats: { hp: 3, def: 4 },
+    icon: 10
+  },
+  {
+    hero: "Eirika (Legendary Heroes)",
+    type: "Water",
+    stats: { hp: 3, atk: 2 },
+    icon: 11
+  },
+  {
+    hero: "Hríd",
+    type: "Wind",
+    stats: { hp: 3, def: 4 },
+    icon: 12
+  },
+  {
+    hero: "Eir",
+    type: "Light",
+    stats: { hp: 5, res: 5 },
+    icon: 13
+  },
+  {
+    hero: "Azura (Legendary Heroes)",
+    type: "Water",
+    stats: { hp: 3, res: 4 },
+    icon: 14
+  },
+  {
+    hero: "Duma",
+    type: "Anima",
+    stats: { hp: 5, atk: 3 },
+    icon: 15
+  },
+  {
+    hero: "Roy (Legendary Heroes)",
+    type: "Fire",
+    stats: { hp: 3 },
+    icon: 16
+  }
+];
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
