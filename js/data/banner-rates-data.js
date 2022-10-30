e863e44892f4f81c09534b6c744054088273dd85
ManaMythii <105987369+ManaMythii@users.noreply.github.com>
2022-10-30 00:05:44 -0500

    Update banner-rates-data.js

--- js/data/banner-rates-data.js
+++ js/data/banner-rates-data.js
@@ -27,7 +27,7 @@ module.exports = {
     pityRateRarity5: 0,
     pityRateRarityFocus: 0.25
   },
-    defaultV3: {
+    defaultV3: {//new heroes 2022
     rateRarity3: 36,
     rateRarity4: 52,
     rateRarityFocus4: 3,
@@ -41,6 +41,21 @@ module.exports = {
     pityRateRarity5: 0.25,
     pityRateRarityFocus: 0.25
   },
+   defaultV4: {//GHB/BHB/VG/Skill Banners 2022
+    rateRarity3: 36,
+    rateRarity4: 55,
+    rateRarityFocus4: 0,
+    rateRaritySpecial4: 3,
+    rateRarity5: 2,
+    rateRarityFocus: 4,
+    pityRateRarity3: -36 / 94 * 0.5,
+    pityRateRarity4: -52 / 94 * 0.5,
+    pityRateRaritySpecial4: -0.02,
+    pityRateRarityFocus4: -0.02,
+    pityRateRarity5: 0.25,
+    pityRateRarityFocus: 0.25
+  },
+  
   herofestV1: {
     rateRarity3: 58,
     rateRarity4: 34,
@@ -77,6 +92,22 @@ module.exports = {
     pityRateRarity5: 0,
     pityRateRarityFocus: 0.5
   },
+  
+   legendaryV2: {//legendary 2022
+    rateRarity3: 34,
+    rateRarity4: 55,
+    rateRarityFocus4: 0,
+    rateRaritySpecial4: 3,
+    rateRarity5: 0,
+    rateRarityFocus: 8,
+    pityRateRarity3: -34 / 92 * 0.5,
+    pityRateRarity4: -58 / 92 * 0.5,
+    pityRateRaritySpecial4: -0.02,
+    pityRateRarityFocus4: 0,
+    pityRateRarity5: 0,
+    pityRateRarityFocus: 0.5
+  },
+  
   focus4V1: {
     rateRarity3: 36,
     rateRarity4: 29,
@@ -88,5 +119,73 @@ module.exports = {
     pityRateRarityFocus4: -29 / 94 * 0.5,
     pityRateRarity5: 0.25,
     pityRateRarityFocus: 0.25
-  }
+  },
+  
+   wklyrevivalV1: {//weekly revival 2022
+    rateRarity3: 36,
+    rateRarity4: 55,
+    rateRarityFocus4: 0,
+    rateRaritySpecial4: 3,
+    rateRarity5: 2,
+    rateRarityFocus: 4,
+    pityRateRarity3: -36 / 94 * 0.5,
+    pityRateRarity4: -52 / 94 * 0.5,
+    pityRateRaritySpecial4: -0.02,
+    pityRateRarityFocus4: 0,
+    pityRateRarity5: 0.25,
+    pityRateRarityFocus: 0.25
+  },
+  
+   dblesplV1: {//double special 2022
+    rateRarity3: 34,
+    rateRarity4: 54,
+    rateRarityFocus4: 3,
+    rateRaritySpecial4: 3,
+    rateRarity5: 0,
+    rateRarityFocus: 6,
+    pityRateRarity3: -36 / 94 * 0.5,
+    pityRateRarity4: -52 / 94 * 0.5,
+    pityRateRaritySpecial4: -0.02,
+    pityRateRarityFocus4: -0.02,
+    pityRateRarity5: 0,
+    pityRateRarityFocus: 0.25
+  },
+  
+   splrevV1: {//special revival 2022
+    rateRarity3: 36,
+    rateRarity4: 58,
+    rateRarityFocus4: 0,
+    rateRaritySpecial4: 0,
+    rateRarity5: 3,
+    rateRarityFocus: 3,
+    pityRateRarity3: -36 / 94 * 0.5,
+    pityRateRarity4: -52 / 94 * 0.5,
+    pityRateRaritySpecial4: 0,
+    pityRateRarityFocus4: 0,
+    pityRateRarity5: 0.25,
+    pityRateRarityFocus: 0.25
+  },
+  
+   mythlegrmxV1: {//mythic and legendary remix 2022
+    rateRarity3: 34,
+    rateRarity4: 57,
+    rateRarityFocus4: 0,
+    rateRaritySpecial4: 3,
+    rateRarity5: 0,
+    rateRarityFocus: 6,
+    pityRateRarity3: -36 / 94 * 0.5,
+    pityRateRarity4: -52 / 94 * 0.5,
+    pityRateRaritySpecial4: -0.02,
+    pityRateRarityFocus4: 0,
+    pityRateRarity5: 0,
+    pityRateRarityFocus: 0.25
+  },
+  
+  
+  
+  
+  
+  
+  
+  
 };
