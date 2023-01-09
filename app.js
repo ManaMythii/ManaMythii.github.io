6f936c54c01ce5978eec063888ebad8b921f1db1
ManaMythii <105987369+ManaMythii@users.noreply.github.com>
2023-01-08 18:51:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ a[5]>=0&&a[0]+a[1]+a[2]+a[3]+a[4]+a[5]<=100?e.rates.rateRarity4=a[5]:e.rates.rat
 let g,t=100-e.rates.rateRarityFocus-e.rates.rateRarity5;0===t?(e.rates.pityRateRarityFocus4=0,e.rates.pityRateRaritySpecial4=0,e.rates.pityRateRaritySpecial4SH=0,e.rates.pityRateRarity4=0,e.rates.pityRateRarity3=0):(e.rates.pityRateRarityFocus4=-e.rates.rateRarityFocus4/t*.5,e.rates.pityRateRaritySpecial4=-e.rates.rateRaritySpecial4/t*.5,e.rates.pityRateRaritySpecial4SH=-e.rates.rateRaritySpecial4SH/t*.5,e.rates.pityRateRarity4=-e.rates.rateRarity4/t*.5,e.rates.pityRateRarity3=-e.rates.rateRarity3/t*.5)}function f(){!function(){let e=$(o.FOCUS_LIST).empty(),a=i.getHeroes(l.focusHeroes);(m=i.getSummoningPool(l.pool,l)).rf=a,
  //j = 4 focus list
 //l.rates.rateRarityFocus4!=0?(j=i.getHeroes(l.focusHeroes4),m.rf4=j,u(o.POOL_LIST_4F,m.rf4)):j=0,
-l.rates.rateRarityFocus4!=0?(j=i.getHeroes(l.focusHeroes4),m.rf4=j):j=0,
+l.rates.rateRarityFocus4!=0?(j=i.getHeroes(l.focusHeroes4),m.rf4=j):m.rf4=[],
 u(o.POOL_LIST_4F,m.rf4),
   
   a.forEach(a=>{$(`<div class="focus-list-hero">\n        <img class="focus-list-hero-frame" src="../img/assets/frame-rarity-5.png">\n        <img class="focus-list-hero-portrait" src="../${a.assets.portrait}">\n        <img class="focus-list-hero-background" src="../img/assets/background-rarity-5.png">\n        </div>`).tooltip({html:!0,placement:"bottom",title:`<p class="mb-0">${a.title}</p><h6>${a.shortName||a.name}</h6>`}).appendTo(e)}),
